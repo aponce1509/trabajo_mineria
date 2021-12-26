a451f74157692b4c24a993fb2061dd9ac1be9ec0
joropostigo99 <96578444+joropostigo99@users.noreply.github.com>
2021-12-26 17:04:58 +0100

    Add files via upload

---
+++ scripts/General_EDA/multiclass.R
@@ -0,0 +1,33 @@
+library(tidyverse)
+
+# Función para extraer las multiclases
+# 1 --> (1,1)
+# 2 --> (1,0)
+# 3 --> (0,1)
+# 4 --> (0,0)
+multiclass = function(x,y) {
+  if (x == 1) {
+    if (y==1) {
+      return(1)
+    } else {
+      return(2)
+    }
+  } else {
+    if (y==1) {
+      return(3)
+    } else {
+      return(4)
+    }
+  }
+}
+
+# Leemos las clases
+classes = as.data.frame(read_csv('training_set_labels.csv'))[,2:3]
+
+# Obtenemos las multiclases aplicando por filas
+training_set_labels_4 =  as.data.frame((classes %>% rowwise() %>% 
+                      mutate(Y=multiclass(h1n1_vaccine,seasonal_vaccine)))[,3])
+
+# Exportamos las etiquetas
+write_csv(training_set_labels_4,'training_set_labels_4.csv')
+
